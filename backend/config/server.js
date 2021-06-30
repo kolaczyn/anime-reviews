cef7d5c7dbcb69e1b37be9dc9454a2ea6ed1ac4b
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-30 22:57:19 +0200

    initial commit - setup Strapi and Gatsby

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b0903418d28fd0de0cf5732cdfebd1c'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97968503-22ec-4556-999a-2c924c43af79'
+};
