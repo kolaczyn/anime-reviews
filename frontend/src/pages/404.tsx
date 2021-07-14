02284eeb4e44701cf3b714d30e162035e29b2787
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-07-14 16:34:48 +0200

    improve 404 page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,13 +1,30 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import ContainerMedium from '../components/layout/container/ContainerMedium';
+import ButtonStyle from '../components/ui/ButtonStyle';
+import { Helmet } from 'react-helmet';
+
+const ERROR_MESSAGE = '404 Page Not Found';
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>404 page not found</h1>
-    </main>
-  )
-}
+    <>
+      <Helmet>
+        <title>{ERROR_MESSAGE} | Anime Reviews</title>
+      </Helmet>
+      <main>
+        <ContainerMedium>
+          <div className="space-y-2">
+            <h1>{ERROR_MESSAGE}</h1>
+            <GatsbyLink className="inline-block" to="/">
+              <ButtonStyle>Back to Home Page</ButtonStyle>
+            </GatsbyLink>
+          </div>
+        </ContainerMedium>
+      </main>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
